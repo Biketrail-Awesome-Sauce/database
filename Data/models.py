f6fc7704288edc3d4c8869547984db0971273d75
boydjohnson <johnson.boyd@gmail.com>
2015-04-06 14:18:41 -0500

    New unmanaged models since they aren't being CRUDed by Django; we have created them ourselves

--- Data/models.py
+++ Data/models.py
@@ -1,71 +1,82 @@
+
+from __future__ import unicode_literals
+
 from django.contrib.gis.db import models
-from django.contrib.gis.geos import GEOSGeometry
-
-
-class MinnesotaBikeTrails(models.Model):
-    ccp_name = models.CharField(max_length=80,blank=True)
-    gf_lyr_nom = models.CharField(max_length=80)
-    one_way = models.IntegerField()
-    rtng_mean = models.FloatField(null=True)
-    rtng_count = models.IntegerField(null=True)
-    rtng_cbf7 = models.FloatField()
-    rtng_ccpx = models.FloatField(null=True)
-    item_tags = models.CharField(max_length=98,blank=True)
-    speedlimit = models.IntegerField(null=True)
-    lane_count = models.IntegerField(null=True)
-    out_ln_wid = models.IntegerField(null=True)
-    shld_width = models.IntegerField(null=True)
-    bike_facil = models.CharField(max_length=80,blank=True)
-    cautionary = models.CharField(max_length=80,blank=True)
-    cycleroute = models.CharField(max_length=80,blank=True)
-    ndl_elev = models.FloatField()
-    ndr_elev = models.FloatField()
-    #the next three need to be named as such because of pgRouting
-    source = models.IntegerField(null=True)
-    target = models.IntegerField(null=True)
-    cost = models.FloatField()
-
-    the_geom = models.LineStringField(srid=4326)
-    objects = models.GeoManager()
-
-    def __str__(self):
-        return self.ccp_name
-
-
-    def get_location(self):
-        return GEOSGeometry(self.the_geom).centroid
-
 
 
 class BestBikeTrails(models.Model):
-    ccp_name = models.CharField(max_length=80,blank=True)
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    ccp_name = models.CharField(max_length=80)
     gf_lyr_nom = models.CharField(max_length=80)
     one_way = models.IntegerField()
-    rtng_mean = models.FloatField(null=True)
-    rtng_count = models.IntegerField(null=True)
+    rtng_mean = models.FloatField(blank=True, null=True)
+    rtng_count = models.IntegerField(blank=True, null=True)
     rtng_cbf7 = models.FloatField()
-    rtng_ccpx = models.FloatField(null=True)
-    item_tags = models.CharField(max_length=98,blank=True)
-    speedlimit = models.IntegerField(null=True)
-    lane_count = models.IntegerField(null=True)
-    out_ln_wid = models.IntegerField(null=True)
-    shld_width = models.IntegerField(null=True)
-    bike_facil = models.CharField(max_length=80,blank=True)
-    cautionary = models.CharField(max_length=80,blank=True)
-    cycleroute = models.CharField(max_length=80,blank=True)
+    rtng_ccpx = models.FloatField(blank=True, null=True)
+    item_tags = models.CharField(max_length=98)
+    speedlimit = models.IntegerField(blank=True, null=True)
+    lane_count = models.IntegerField(blank=True, null=True)
+    out_ln_wid = models.IntegerField(blank=True, null=True)
+    shld_width = models.IntegerField(blank=True, null=True)
+    bike_facil = models.CharField(max_length=80)
+    cautionary = models.CharField(max_length=80)
+    cycleroute = models.CharField(max_length=80)
     ndl_elev = models.FloatField()
     ndr_elev = models.FloatField()
-    #the next three need to be named as such because of pgRouting
-    source = models.IntegerField(null=True)
-    target = models.IntegerField(null=True)
+    source = models.IntegerField(blank=True, null=True)
+    target = models.IntegerField(blank=True, null=True)
     cost = models.FloatField()
-
-    the_geom = models.LineStringField(srid=4326)
+    the_geom = models.LineStringField()
     objects = models.GeoManager()
 
-    def __str__(self):
-        return self.ccp_name
+    class Meta:
+        managed = False
+        db_table = 'Data_bestbiketrails'
 
 
-    def get_location(self):
-        return GEOSGeometry(self.the_geom).centroid
+class Bikeintersections(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    cnt = models.IntegerField(blank=True, null=True)
+    chk = models.IntegerField(blank=True, null=True)
+    ein = models.IntegerField(blank=True, null=True)
+    eout = models.IntegerField(blank=True, null=True)
+    the_geom = models.PointField(blank=True, null=True)
+    name = models.CharField(max_length=130, blank=True)
+    objects = models.GeoManager()
+
+    class Meta:
+        managed = False
+        db_table = 'Data_bikeintersections'
+
+
+class MinnesotaBikeTrails(models.Model):
+    id = models.IntegerField(primary_key=True)  # AutoField?
+    ccp_name = models.CharField(max_length=80)
+    gf_lyr_nom = models.CharField(max_length=80)
+    one_way = models.IntegerField()
+    rtng_mean = models.FloatField(blank=True, null=True)
+    rtng_count = models.IntegerField(blank=True, null=True)
+    rtng_cbf7 = models.FloatField()
+    rtng_ccpx = models.FloatField(blank=True, null=True)
+    item_tags = models.CharField(max_length=98)
+    speedlimit = models.IntegerField(blank=True, null=True)
+    lane_count = models.IntegerField(blank=True, null=True)
+    out_ln_wid = models.IntegerField(blank=True, null=True)
+    shld_width = models.IntegerField(blank=True, null=True)
+    bike_facil = models.CharField(max_length=80)
+    cautionary = models.CharField(max_length=80)
+    cycleroute = models.CharField(max_length=80)
+    ndl_elev = models.FloatField()
+    ndr_elev = models.FloatField()
+    source = models.IntegerField(blank=True, null=True)
+    target = models.IntegerField(blank=True, null=True)
+    cost = models.FloatField()
+    the_geom = models.LineStringField()
+    objects = models.GeoManager()
+
+    class Meta:
+        managed = False
+        db_table = 'Data_minnesotabiketrails'
+
+
+
