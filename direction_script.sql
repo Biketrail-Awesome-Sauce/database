f1c58243ca84a9a5eceffd8d213eb0601b22a256
boydjohnson <lt359mn38>
2015-11-24 15:05:19 -0600

    The database took the plpgsql and created a function.

--- direction_script.sql
+++ direction_script.sql
@@ -1,9 +1,17 @@
-CREATE OR REPLACE FUNCTION directions(geometry, geometry ) RETURNS INTEGER as $$
-BEGIN
-  DECLARE angle1 float;
-  DECLARE angle2 float;
-  SET angle1 = ST_AZIMUTH(ST_STARTPOINT($1), ST_ENDPOINT($1))*180/pi();
-  SET angle2 = ST_AZIMUTH(ST_STARTPOINT($2), ST_ENDPOINT($2))*180/pi();
+CREATE OR REPLACE FUNCTION directions(line1 geometry, line2 geometry)
+  RETURNS INTEGER AS $$
+  DECLARE angle1 FLOAT := ST_AZIMUTH (ST_STARTPOINT(line1), ST_ENDPOINT(line1))*180/pi();
+  DECLARE angle2 FLOAT := ST_AZIMUTH (ST_STARTPOINT(line2), ST_ENDPOINT(line2))*180/pi();
 
-  if ABS(angle1-angle2) < 15 THEN SELECT 1;
-  ELSE IF 
+BEGIN
+  IF (ABS(angle1 - angle2) < 15) THEN RETURN 0; --Straight
+  ELSIF (angle1-angle2 > 45) AND (angle1-angle2 < 75) THEN RETURN 1; --Slight Left
+  ELSIF (angle1-angle2 >= 75) AND (angle1-angle2 < 100) THEN RETURN 2;  --Left
+  ELSIF (angle1-angle2 >=100) THEN RETURN 3;  --Sharp left
+  ELSIF (angle2 - angle1 > 45) AND (angle2-angle1 < 75) THEN RETURN 4; --Slight right
+  ELSIF (angle2 - angle1 >= 75) AND (angle2-angle1 < 100) THEN RETURN 5; --Right
+  ELSIF (angle2 - angle1 >= 100) THEN RETURN 6; --Sharp Right
+  ELSE RETURN -1;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
